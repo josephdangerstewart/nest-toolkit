afca8623d1ae314ad0d7fa6d1e636520e57f0b6a
Joseph Stewart <joseph.s@nationalschoolproject.com>
2021-01-12 22:07:42 -0800

    Add swagger tags to auth controller

--- modules/auth/Auth.controller.ts
+++ modules/auth/Auth.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, UseGuards, Inject, Res, Redirect } from '@nestjs/common';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { GoogleLoginGuard } from './GoogleLogin.guard';
 import { AUTH_CONTROLLER_OPTIONS } from './provideConstants';
 
@@ -7,11 +8,15 @@ export interface AuthControllerOptions {
 }
 
 @Controller()
+@ApiTags('auth')
 export class AuthController {
 	@Inject(AUTH_CONTROLLER_OPTIONS)
 	private options: AuthControllerOptions;
 
 	@Get('/auth/google')
+	@ApiOperation({
+		description: 'Hit this route to authenticate with the api'
+	})
 	@UseGuards(GoogleLoginGuard)
 	// eslint-disable-next-line @typescript-eslint/no-empty-function
 	googleAuth() { }
