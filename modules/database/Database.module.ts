c54156775ac417aecb7d30c5d430c8f3b899f4fc
Joseph Stewart <joseph.s@nationalschoolproject.com>
2021-02-10 19:29:51 -0800

    Provide interceptor

--- modules/database/Database.module.ts
+++ modules/database/Database.module.ts
@@ -1,12 +1,21 @@
 import { Module, Global, DynamicModule } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core'; 
 import { DatabaseService, DatabaseServiceOptions } from './Database.service';
 import { DatabasePoolService } from './DatabasePool.service';
+import { DatabaseInterceptor } from './Database.interceptor';
 import { DATABASE_OPTIONS } from './provideConstants';
 
 @Global()
 @Module({
-	providers: [DatabaseService, DatabasePoolService],
-	exports: [DatabaseService]
+	providers: [
+		DatabaseService,
+		DatabasePoolService,
+		{
+			provide: APP_INTERCEPTOR,
+			useClass: DatabaseInterceptor,
+		}
+	],
+	exports: [DatabaseService],
 })
 export class DatabaseModule {
 	static register(options: DatabaseServiceOptions): DynamicModule {
