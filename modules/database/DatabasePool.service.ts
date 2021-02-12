5a69312d839dc459db69728444520680b425d250
Joseph Stewart <joseph.s@nationalschoolproject.com>
2021-02-12 07:06:37 -0800

    Log open connections on error

--- modules/database/DatabasePool.service.ts
+++ modules/database/DatabasePool.service.ts
@@ -82,7 +82,7 @@ export class DatabasePoolService {
 
 	async onModuleDestroy() {
 		if (this.connectionsToClose.length !== 0) {
-			console.error('Not all connections were released before app shutdown');
+			console.error(`Not all connections were released before app shutdown: ${this.connectionsToClose.length} open connections`);
 
 			for (const connection of this.connectionsToClose) {
 				connection.dispose();
@@ -91,7 +91,7 @@ export class DatabasePoolService {
 
 		const requestScopedConnections = Object.values(this.openConnections).filter(Boolean);
 		if (requestScopedConnections.length !== 0) {
-			console.error('Not all request scoped connections were released before app shutdown');
+			console.error(`Not all request scoped connections were released before app shutdown: ${requestScopedConnections} open connections`);
 			
 			for (const connection of requestScopedConnections) {
 				connection.dispose();
