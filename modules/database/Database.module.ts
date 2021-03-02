4843d772906b9c4d34ea0d62f5a2c9fba8f04c98
Joseph Stewart <joseph.s@nationalschoolproject.com>
2021-03-01 17:21:01 -0800

    Import whole logging module

--- modules/database/Database.module.ts
+++ modules/database/Database.module.ts
@@ -4,7 +4,7 @@ import { DatabaseService, DatabaseServiceOptions } from './Database.service';
 import { DatabasePoolService } from './DatabasePool.service';
 import { DatabaseInterceptor } from './Database.interceptor';
 import { DATABASE_OPTIONS, DATABASE_POOL } from './provideConstants';
-import { LoggingService } from '../logging';
+import { LoggingModule } from '../logging';
 
 @Global()
 @Module({
@@ -26,7 +26,7 @@ import { LoggingService } from '../logging';
 			useClass: DatabasePoolService,
 		},
 	],
-	imports: [LoggingService]
+	imports: [LoggingModule]
 })
 export class DatabaseModule {
 	static register(options: DatabaseServiceOptions): DynamicModule {
